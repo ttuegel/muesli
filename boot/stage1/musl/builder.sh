893487f39e98c028d425276b9c8e92c7e3cc1227
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-08 17:23:34 -0500

    boot/stage1/musl: Install specs for gcc

--- boot/stage1/musl/builder.sh
+++ boot/stage1/musl/builder.sh
@@ -4,6 +4,12 @@ unpackTar "${src:?}" musl
 cd musl
 
 
+# Patch
+
+## Don't assume sh is in PATH
+sed -i Makefile -e 's|\tsh |\tdash |'
+
+
 # Configure
 
 ./configure \
@@ -22,3 +28,13 @@ make
 # Install
 
 make install
+make "${out:?}/lib/musl-gcc.specs"
+
+
+# Fixup
+
+mkdir -pv "${out:?}/config/host"
+echo "-specs ${out:?}/lib/musl-gcc.specs" \
+     >> "${out:?}/config/host/CFLAGS"
+echo "-specs ${out:?}/lib/musl-gcc.specs" \
+     >> "${out:?}/config/host/CXXFLAGS"
