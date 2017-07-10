24370edfa4bce39974ed6c9c9c6df6978e10deb4
Thomas Tuegel <ttuegel@mailbox.org>
2017-07-10 08:52:20 -0500

    boot/stage1/libstdcxx: Quote `configure' arguments

--- boot/stage1/libstdcxx/builder.sh
+++ boot/stage1/libstdcxx/builder.sh
@@ -13,14 +13,14 @@ gxx_include_dir="${out:?}/${target:?}/include/c++/${version:?}"
 
 ../libstdc++-v3/configure \
     --prefix="${out:?}" \
-    --target=${target:?} \
-    --host=${host:?} \
-    --build=${build:?} \
+    --target="${target:?}" \
+    --host="${host:?}" \
+    --build="${build:?}" \
     --disable-multilib \
     --disable-nls \
     --disable-libstdcxx-threads \
     --disable-libstdcxx-pch \
-    --with-gxx-include-dir="$gxx_include_dir"
+    --with-gxx-include-dir="${gxx_include_dir}"
 
 
 # Build
